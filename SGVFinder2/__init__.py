1bc794358ce84b8eb154680ff0bd4ccdb061d792
Yoli Meydan % Korem <ym2877@cumc.columbia.edu>
2024-01-16 10:53:51 -0500

    Added checks for bowtie2 and samtools installation

--- SGVFinder2/__init__.py
+++ SGVFinder2/__init__.py
@@ -1,3 +1,7 @@
 from .icra import single_file
 from .svfinder import get_sample_map, work_on_collection
 from .createdb import create_db_from_reps
+from distutils.spawn import find_executable
+
+assert find_executable('bowtie2') is not None, 'Cannot find `bowtie2` installation'
+assert find_executable('samtools') is not None, 'Cannot find `samtools` installation'
